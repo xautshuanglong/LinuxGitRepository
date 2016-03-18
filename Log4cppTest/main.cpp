712d55beb28205bdbb6ed11b165b4eadec82f203
shuanglong <xjshuanglong@126.com>
2016-03-18 16:36:38 +0800

    add libtool and modified main cpp

--- Log4cppTest/main.cpp
+++ Log4cppTest/main.cpp
@@ -4,11 +4,29 @@
 #include <log4cpp/Category.hh>
 #include <log4cpp/PatternLayout.hh>
 
-int main()
+using namespace std;
+using namespace log4cpp;
+
+int main(int argc, char* argv[])
 {
-    std::cout<<"test.cpp"<<std::endl;
-	log4cpp::BasicConfigurator::configure();
-	log4cpp::Category::shutdown();
+    cout<< argv[0] << endl;
+	BasicConfigurator::configure();
+	PatternLayout* layout = new PatternLayout();
+	Category::getRoot().getAppender()->setLayout(layout);
+	Category& cat = Category::getInstance("cat1");
+
+	layout->setConversionPattern("%d{%Y-%m-%d %H:%M:%S.%l} [%-5p] %c %m%n");
+
+	cat.setPriority(Priority::DEBUG);
+
+	cat.info("info testing");
+	cat.debug("debug testing");
+	cat.error("debug testing");
+	cat.fatal("debug testing");
+	cat.warn("debug testing");
+	cat.emerg("debug testing");
+
+	Category::shutdown();
 
     return 0;
 }
