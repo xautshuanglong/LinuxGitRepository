b142e48ecd23ed2bd20e0e76dde7793c9133dfa8
shuanglong <xjshuanglong@126.com>
2016-03-28 23:39:03 +0800

    M log4cpp.init

--- Log4cppConfigTest/Log4cppConfig.cpp
+++ Log4cppConfigTest/Log4cppConfig.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 {
     try
     {
-        PropertyConfigurator::configure(".//config//log4cpp.ini"); 
+        PropertyConfigurator::configure(".//config//log4cpp.init");
     }
     catch (ConfigureFailure& e)
     {
@@ -24,15 +24,20 @@ int main(int argc, char* argv[])
 
     NDC::push("Log4cppConfigTest");
 
-    sub1.info("sub1 info testing");
-    sub1.debug("sub1 info testing");
-    sub1.warn("sub1 info testing");
-    sub1.error("sub1 info testing");
+    root.info("root info testing");
+    root.debug("root debug testing");
+    root.warn("root warn testing");
+    root.error("root error testing");
 
-    sub2.info("sub2 alert testing");
-    sub2.debug("sub1 info testing");
-    sub2.warn("sub1 info testing");
-    sub2.error("sub1 info testing");
+    sub1.info("sub1 info testing");
+    sub1.debug("sub1 debug testing");
+    sub1.warn("sub1 warn testing");
+    sub1.error("sub1 error testing");
+
+    sub2.info("sub2 info testing");
+    sub2.debug("sub2 debug testing");
+    sub2.warn("sub2 warn testing");
+    sub2.error("sub2 error testing");
 
     return 0;
 }
