1f86cf93ce4eb91271fcf1706502ab44b9dde2ea
xautshuanglong <xjshuanglong@126.com>
2017-12-03 23:55:24 +0800

    add main.cpp from UbuntuServer

---
+++ ShuanglongServer/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "config.h"
+#include "utils/LogUtil.h"
+#include "utils/StringUtil.h"
+
+using namespace Shuanglong::Utils;
+
+int main(int argc, char **argv)
+{
+    std::cout << "--------------------- ShuanglongServer -------------------------" << std::endl;
+
+    LogUtil log;
+    StringUtil strutil;
+    
+    std::cout << "---------------------------- END -------------------------------" << std::endl;
+    
+    // warning: nested namespace definition is a C++1z extension; define each namespace separately [-Wc++1z-extensions]
+
+#if DEBUG
+    std::cout << "define DEBUG when excute configure" << std::endl;
+#else
+    std::cout << "do not define DEBUG" << std::endl;
+#endif
+
+    return 0;
+}
