9aa7c03c36a08aba914d37400d5ccd03783d395a
xautshuanglong <xjshuanglong@126.com>
2017-12-12 23:58:21 +0800

    add TestDemo source for makefile and gdb testing

---
+++ TestDemo/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "======================= Main ======================" <<std::endl;
+    std::cout << "======================= Main Line 2 ======================" <<std::endl;
+
+    return 0;
+}
+
