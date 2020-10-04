24812d035761a18cf1c958b2f7ee16a1fde2485b
LinuxShuanglong <xjshuanglong@126.com>
2020-10-05 00:52:53 +0800

    gdb go 测试

--- GoLearning/main.go
+++ GoLearning/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "strconv"
+)
 
 func main() {
+    for index, arg := range os.Args {
+        fmt.Println("Args[" + strconv.Itoa(index) + "] = ", arg)
+    }
+
+    var argCount int = len(os.Args)
+    if argCount > 2 {
+        fmt.Println("Hello")
+    } else {
+        fmt.Println("World")
+    }
+
     fmt.Printf("Hello, world!\n")
 }
+
