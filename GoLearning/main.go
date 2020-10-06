68565ae94a1535c3bc7cc93a60e160b0138b97fb
LinuxShuanglong <xjshuanglong@126.com>
2020-10-06 15:32:48 +0800

    tab 转 空格 失败

--- GoLearning/main.go
+++ GoLearning/main.go
@@ -1,22 +1,30 @@
+//
+// Author: xautshuanglong
+// Date: 2020-10-06 14:51:34
+// LastEditor: xautshuanglong
+// LastEditTime: 2020-10-06 15:30:40
+// FilePath: /GoLearning/main.go
+// Description:
+//
 package main
 
 import (
-    "fmt"
-    "os"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-    for index, arg := range os.Args {
-        fmt.Println("Args[" + strconv.Itoa(index) + "] = ", arg)
-    }
+	for index, arg := range os.Args {
+		fmt.Println("Args["+strconv.Itoa(index)+"] = ", arg)
+	}
 
-    var argCount int = len(os.Args)
-    if argCount > 2 {
-        fmt.Println("Hello")
-    } else {
-        fmt.Println("World")
-    }
+	var argCount int = len(os.Args)
+	if argCount > 2 {
+		fmt.Println("Hello")
+	} else {
+		fmt.Println("World")
+	}
 
-    fmt.Printf("Hello, world!\n")
+	fmt.Printf("Hello, world!\n")
 }
---
+++ GoLearning/testing/test_multithread.go
@@ -0,0 +1,23 @@
+//
+// Author: xautshuanglong
+// Date: 2020-10-06 11:13:42
+// LastEditor: xautshuanglong
+// LastEditTime: 2020-10-06 15:31:10
+// FilePath: /GoLearning/testing/test_multithread.go
+// Description:
+//
+
+package testing
+
+import (
+	"fmt"
+)
+
+// TestEntry sf
+// description:
+// param {type}
+// return {type}
+//
+func TestEntry() {
+	fmt.Println("-------------------- testing TestEntry --------------------")
+}
