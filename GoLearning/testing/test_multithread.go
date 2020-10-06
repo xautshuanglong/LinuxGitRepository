2ce1de808c06bb85d8f8db8c76cfb9c2a004a213
LinuxShuanglong <xjshuanglong@126.com>
2020-10-06 20:18:01 +0800

    修改注释模板

--- GoLearning/testing/test_multithread.go
+++ GoLearning/testing/test_multithread.go
@@ -1,23 +1,23 @@
-//
-// Author: xautshuanglong
-// Date: 2020-10-06 11:13:42
-// LastEditor: xautshuanglong
-// LastEditTime: 2020-10-06 15:31:10
-// FilePath: /GoLearning/testing/test_multithread.go
-// Description:
-//
+/********************************************************************\
+*  Author: xautshuanglong
+*  Date: 2020-10-06 11:13:42
+*  LastEditor: xautshuanglong
+*  LastEditTime: 2020-10-06 20:16:53
+*  FilePath: /GoLearning/testing/test_multithread.go
+*  Description: 
+\********************************************************************/
 
 package testing
 
 import (
-	"fmt"
+    "fmt"
 )
 
-// TestEntry sf
-// description:
-// param {type}
-// return {type}
-//
+/********************************************************************\
+*  description: 
+*  param {type} 
+*  return {type} 
+\********************************************************************/
 func TestEntry() {
-	fmt.Println("-------------------- testing TestEntry --------------------")
+    fmt.Println("-------------------- testing TestEntry --------------------")
 }
