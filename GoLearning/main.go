9bf60cf8160bc18c3e6da523ca02f7522cf3751c
LinuxShuanglong <xjshuanglong@126.com>
2020-10-10 17:06:35 +0800

    flag 使用测试

--- GoLearning/main.go
+++ GoLearning/main.go
@@ -2,32 +2,49 @@
 *  Author: xautshuanglong
 *  Date: 2020-10-06 15:36:02
 *  LastEditor: xautshuanglong
-*  LastEditTime: 2020-10-10 00:58:05
+*  LastEditTime: 2020-10-10 17:04:35
 *  FilePath: /GoLearning/main.go
 *  Description:
 \********************************************************************/
 package main
 
 import (
+    "./test_demo"
+    "flag"
     "fmt"
     "os"
     "strconv"
 )
-import "./test_demo"
+
+var cliName = flag.String("name", "nick", "Input your name")
+var cliAge = flag.Int("age", 28, "Input Your Age")
+var cliGender = flag.String("gender", "male", "Input Your Gender")
+
+var cliFlag int
+
+func Init() {
+    flag.IntVar(&cliFlag, "flagname", 1234, "Just for demo")
+}
 
 func main() {
+    Init()
+    flag.Parse()
+    // flag.Args() 函数返回没有被解析的命令行参数
+    // func NArg() 函数返回没有被解析的命令行参数的个数
+    fmt.Printf("args=%s, num=%d\n", flag.Args(), flag.NArg())
+    for i := 0; i != flag.NArg(); i++ {
+        fmt.Printf("arg[%d]=%s\n", i, flag.Arg(i))
+    }
+
+    // 输出命令行参数
+    fmt.Println("name=", *cliName)
+    fmt.Println("age=", *cliAge)
+    fmt.Println("gender=", *cliGender)
+    fmt.Println("flagname=", cliFlag)
+
     for index, arg := range os.Args {
         fmt.Println("Args["+strconv.Itoa(index)+"] = ", arg)
     }
-
-    var argCount int = len(os.Args)
-    if argCount > 2 {
-        fmt.Println("Hello")
-    } else {
-        fmt.Println("World")
-    }
-
-    fmt.Printf("Hello, world!\n")
 
     test_demo.TestInit()
     test_demo.TestEntry()
