ce291bae4cde1d656e4fa303941bcd91c6e67537
JiangShuanglong <xjshuanglong@125.com>
2020-10-18 11:40:56 +0800

    添加 go 切片测试入口

---
+++ GoLearning/test_demo/test_slice.go
@@ -0,0 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-18 02:03:53
+ * @LastEditTime: 2020-10-18 10:32:57
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /GoLearning/test_demo/test_slice.go
+ */
+package test_demo
+
+import (
+    "fmt"
+)
+
+func SliceTest() {
+    fmt.Print("")
+}
