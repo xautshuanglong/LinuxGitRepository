03ed5ffa658d6f61edd1197c5f7af8ab94464f24
LinuxShuanglong <xjshuanglong@126.com>
2020-10-19 15:30:06 +0800

    借助 figlet 工具生成标语

--- TestDemo/Main.cpp
+++ TestDemo/Main.cpp
@@ -5,7 +5,7 @@ void ShowDescription();
 
 int main(int argc, char *argv[])
 {
-//    ShowDescription();
+    ShowDescription();
     
     Shuanglong::Application app;
     return app.Run(argc, argv);
@@ -13,18 +13,14 @@ int main(int argc, char *argv[])
 
 void ShowDescription()
 {
-    std::cout << "  .--------. .--.                                                      .--.                                         " << std::endl;
-    std::cout << " /  .---.  : |  |                                                      |  |                                         " << std::endl;
-    std::cout << " |  |   '--' |  |                                                      |  |                                         " << std::endl;
-    std::cout << " \\  '-----.  |  '------. .--.   .--.   .----.     .-----.    .------.  |  |        .----.     .-----.    .------.   " << std::endl;
-    std::cout << "  '----.   ' |  .---.  | |  |   |  |  /  __  \\   /  .-.  \\  /   __   \\ |  |       /  __  \\   /  .-.  \\  /   __   \\  " << std::endl;
-    std::cout << "        |  | |  |   |  | |  |   |  | |  /  \\  | |  /   \\  | |  /  \\  | |  |      |  /  \\  | |  /   \\  | |  /  \\  |  " << std::endl; 
-    std::cout << " .--.   |  | |  |   |  | |  |   |  | |  \\__/  | |  |   |  | |  \\__/  | |  |      |  \\__/  | |  |   |  | |  \\__/  |  " << std::endl; 
-    std::cout << " :  '---'  ; |  |   |  | |  '---'  |  \\       | |  |   |  | \\        | |  '-----. \\      /  |  |   |  | \\        |  " << std::endl; 
-    std::cout << "  '-------'  '--'   '--' '------'--'   '----'-' '--'   '--'  '----|  | '--------'  '----'   '--'   '--'  '----|  |  " << std::endl; 
-    std::cout << "                                                                  |  |                                        |  |  " << std::endl; 
-    std::cout << "                                                            .-.   ;  |                                  .-.   ;  |  " << std::endl;
-    std::cout << "                                                            \\  '-'   /                                  \\  '-'   /  " << std::endl;
-    std::cout << "                                                             '------'                                    '------'   " << std::endl;
+    // sudo apt install figlet
+    // figlet -f slant shuanglong
+
+    std::cout << "         __                            __                  " << std::endl;
+    std::cout << "   _____/ /_  __  ______ _____  ____ _/ /___  ____  ____ _ " << std::endl;
+    std::cout << "  / ___/ __ \\/ / / / __ `/ __ \\/ __ `/ / __ \\/ __ \\/ __ `/ " << std::endl;
+    std::cout << " (__  ) / / / /_/ / /_/ / / / / /_/ / / /_/ / / / / /_/ /  " << std::endl;
+    std::cout << "/____/_/ /_/\\__,_/\\__,_/_/ /_/\\__, /_/\\____/_/ /_/\\__, /   " << std::endl;
+    std::cout << "                             /____/              /____/    " << std::endl;
 }
 
