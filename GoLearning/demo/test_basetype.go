da27b95528bee8c1a599970b3944fec8210ea465
JiangShuanglong <xjshuanglong@125.com>
2020-11-15 22:05:53 +0800

    基本类型补充：数组和字符串

--- GoLearning/demo/test_basetype.go
+++ GoLearning/demo/test_basetype.go
@@ -2,8 +2,8 @@
  *  Author: xautshuanglong
  *  Date: 2020-10-20 14:51:29
  *  LastEditor: xautshuanglong
- *  LastEditTime: 2020-10-29 22:08:10
- *  FilePath: /GoLearning/test_demo/test_basetype.go
+ *  LastEditTime: 2020-11-15 22:04:25
+ *  FilePath: /GoLearning/demo/test_basetype.go
  *  Description:
 \********************************************************************/
 
@@ -11,11 +11,188 @@ package test_demo
 
 import (
     "fmt"
+    "reflect"
+    "unicode/utf8"
+    "unsafe"
 )
 
 func BaseType_TestEntry() {
-    BaseType_Slice()
-    BaseType_Rename()
+    // BaseType_Array()
+    BaseType_String()
+    // BaseType_Slice()
+    // BaseType_Rename()
+}
+
+func BaseType_Array() {
+    var a [3]int
+    var b = [...]int{1, 2, 3}
+    var c = [...]int{2: 3, 1: 2}
+    var d = [...]int{1, 2, 4: 5, 6}
+
+    fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a) // len=3 cap=3 value=[0 0 0]
+    fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b) // len=3 cap=3 value=[1 2 3]
+    fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c) // len=3 cap=3 value=[0 2 3]
+    fmt.Printf("len=%d cap=%d value=%v\n", len(d), cap(d), d) // len=6 cap=6 value=[1 2 0 0 5 6]
+
+    var original = [...]int{1, 2, 3}
+    fmt.Printf("original = %v\n", original)
+
+    // 数组拷贝
+    var copy1 = original // 原数组拷贝
+    copy1[1] = 5         // 不改变原数组
+    fmt.Printf("original=%v  copy1=%v len(copy1)=%v\n", original, copy1, len(copy1))
+
+    // 数组指针
+    var copy2 = &original // 指向原数组的指针
+    copy2[1] = 7          // 修改原数组的值
+    fmt.Printf("original=%v  copy2=%v len(copy2)=%v\n", original, copy2, len(copy2))
+
+    // 数组遍历方法
+    for i, v := range original {
+        fmt.Printf("original[%v] = %v\n", i, v)
+    }
+    for i := range copy2 {
+        fmt.Printf("copy2[%d] = %d\n", i, copy2[i])
+    }
+    for i := 0; i < len(original); i++ {
+        fmt.Printf("original[%d] = %d\n", i, original[i])
+    }
+
+    // 二维数组
+    var dimension_2 [5][5]int
+    for i := range dimension_2 {
+        fmt.Printf("dimension_2[%d] = %v\n", i, dimension_2[i])
+    }
+
+    // 长度为零的数组
+    var e [0]int
+    var f = [0]int{}
+    var g = [...]int{}
+    fmt.Printf("len=%d cap=%d value=%v\n", len(e), cap(e), e)
+    fmt.Printf("len=%d cap=%d value=%v\n", len(f), cap(f), f)
+    fmt.Printf("len=%d cap=%d value=%v\n", len(g), cap(g), g)
+}
+
+func BaseType_String() {
+    var data = [...]byte{
+        'h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!',
+    }
+    fmt.Printf("data=%v  len=%v\n", data, len(data))
+    str := "hello, world!"
+    fmt.Printf("str=%v  len=%v\n", str, len(str))
+    str1 := str[:5]
+    fmt.Printf("str1=%s  len=%v\n", str1, len(str1))
+    str2 := str[7:12]
+    fmt.Printf("str2=%s  len=%v\n", str2, len(str2))
+
+    fmt.Printf("len(str)=%v\n", (*reflect.StringHeader)(unsafe.Pointer(&str)).Len)
+    fmt.Printf("len(str1)=%v\n", (*reflect.StringHeader)(unsafe.Pointer(&str1)).Len)
+    fmt.Printf("len(str2)=%v\n", (*reflect.StringHeader)(unsafe.Pointer(&str2)).Len)
+
+    bytes := []byte("Hello, 世界")
+    fmt.Printf("bytes=%#v\n", bytes)
+    fmt.Println("\"\\xe4\\xb8\\x96\\xe7\\x95\\x8c\" = ", "\xe4\xb8\x96\xe7\x95\x8c")
+    for i, c := range bytes {
+        fmt.Printf("bytes[%d]=%v\n", i, c)
+    }
+    for i, c := range "\xe4\xb8\x96\xe7\x95\x8c" {
+        fmt.Printf("const_bytes[%d]=%v (%s)\n", i, c, string(c))
+    }
+    fmt.Printf("rune_test=%#v\n", []rune("世界"))             // []int32{19990, 30028}
+    fmt.Printf("rune_test=%#v\n", string([]rune{'世', '界'})) // 世界
+
+    // for range string 模拟实现
+    utf8String := "hello, 世界!"
+    forOnString(utf8String, func(i int, r rune) {
+        fmt.Printf("simulate range utf8String[%d] = %v\n", i, r)
+    })
+
+    // []byte(string) 模拟实现
+    utf8Bytes := str2bytes(utf8String)
+    utf8Bytes2 := []byte(utf8String)
+    for i, c := range utf8Bytes {
+        fmt.Printf("utf8Bytes[%d]=%v \t (%s) \t utf8Bytes2[%d]=%v\n", i, c, string(c), i, utf8Bytes2[i])
+    }
+
+    // string([]byte) 模拟实现
+    var testBytesConst = [...]byte{
+        'h', 'e', 'l', 'l', 'o', ',', ' ', 0xe4, 0xb8, 0x96, 0xe7, 0x95, 0x8c, 33,
+    }
+    var testBytes = make([]byte, len(testBytesConst))
+    for i := range testBytesConst {
+        testBytes[i] = testBytesConst[i]
+    }
+    byte2string := bytes2str(testBytes)
+    fmt.Println("btye2string = ", byte2string)
+
+    // []rune(string) 模拟实现
+    var str2rune []rune = str2runes(utf8String)
+    for i, rune := range str2rune {
+        fmt.Printf("str2rune[%d]=%-10v (%s)\n", i, rune, string(rune))
+    }
+
+    // string([]rune) 模拟实现
+    var runesConst = [...]rune{
+        'h', 'e', 'l', 'l', 'o', ',', ' ', 19990, 30028, 33,
+    }
+    var runes = make([]rune, len(runesConst))
+    for i, rune := range runesConst {
+        runes[i] = rune
+    }
+    var rune2string = runes2string(runes)
+    fmt.Printf("string2rune=%s\n", rune2string)
+}
+
+func forOnString(s string, forBody func(i int, r rune)) {
+    for i := 0; len(s) > 0; {
+        r, size := utf8.DecodeRuneInString(s)
+        forBody(i, r)
+        s = s[size:]
+        i += size
+    }
+}
+
+func str2bytes(s string) []byte {
+    p := make([]byte, len(s))
+    for i := 0; i < len(s); i++ {
+        c := s[i]
+        p[i] = c
+    }
+    return p
+}
+
+func bytes2str(s []byte) (p string) {
+    data := make([]byte, len(s))
+    copy(data, s)
+    // for i, c := range s {
+    //     data[i] = c
+    // }
+
+    hdr := (*reflect.StringHeader)(unsafe.Pointer(&p))
+    hdr.Data = uintptr(unsafe.Pointer(&data[0]))
+    hdr.Len = len(s)
+
+    return p
+}
+
+func str2runes(s string) []rune {
+    var p []int32
+    for len(s) > 0 {
+        r, size := utf8.DecodeRuneInString(s)
+        p = append(p, int32(r))
+        s = s[size:]
+    }
+    return []rune(p)
+}
+
+func runes2string(s []int32) string {
+    var p []byte
+    buf := make([]byte, 3)
+    for _, r := range s {
+        n := utf8.EncodeRune(buf, r)
+        p = append(p, buf[:n]...)
+    }
+    return string(p)
 }
 
 func BaseType_Slice() {
@@ -52,7 +229,6 @@ func BaseType_Slice() {
     /* 打印子切片从索引 2(包含) 到索引 5(不包含) */
     number3 := numbers[2:5]
     printSlice(number3)
-
 }
 
 func printSlice(x []int) {
