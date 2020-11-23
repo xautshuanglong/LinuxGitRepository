0418727a1813328b5840696a3ca1ab12c67a1faf
LinuxShuanglong <xjshuanglong@126.com>
2020-11-23 17:14:22 +0800

    if 语句中使用分号，分号前部分为初始化语句，分号后部分作为判断条件。

--- GoLearning/demo/test_exception.go
+++ GoLearning/demo/test_exception.go
@@ -2,7 +2,7 @@
  *  Author: xautshuanglong
  *  Date: 2020-11-23 14:41:54
  *  LastEditor: xautshuanglong
- *  LastEditTime: 2020-11-23 14:54:39
+ *  LastEditTime: 2020-11-23 17:09:52
  *  FilePath: /GoLearning/demo/test_exception.go
  *  Description:
 \********************************************************************/
@@ -10,11 +10,13 @@
 package demo
 
 import (
+    "errors"
     "fmt"
 )
 
 func Exception_TestEntry() {
     Exception_MapError()
+    // Exception_RetriveError()
 }
 
 func Exception_MapError() {
@@ -23,9 +25,38 @@ func Exception_MapError() {
 }
 
 func mapError() string {
-    var m map[string]string
+    var m = map[string]string{}
+    // m["key"] = "value"
     if v, ok := m["key"]; ok {
         return v
     }
-    return ""
+
+    var a bool = true
+    var b bool = false
+    if c := a == b; a {
+        fmt.Println("if line 1 ...  a,b,c =", a, b, c)
+    }
+    if c := a == b; b {
+        fmt.Println("if line 2 ...  a,b,c =", a, b, c)
+    }
+    if c := a == b; c {
+        fmt.Println("if line 3 ...  a,b,c =", a, b, c)
+    }
+    return "null"
+}
+
+func Exception_RetriveError() {
+    value, err := getEvenNum(3)
+    if err == nil {
+        fmt.Println("Value =", value)
+    } else {
+        fmt.Println("getEvenNum failed!")
+    }
+}
+
+func getEvenNum(num int) (int, error) {
+    if num%2 == 0 {
+        return num, nil
+    }
+    return 0, errors.New(fmt.Sprintf("the value %d is not even number", num))
 }
