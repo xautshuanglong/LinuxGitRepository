c91894921a5dbf648b703429f942eda3cca64640
JiangShuanglong <xjshuanglong@125.com>
2021-02-19 00:59:29 +0800

    获取系统时间测试

--- TestDemo/Utils/TimeUtil.cpp
+++ TestDemo/Utils/TimeUtil.cpp
@@ -11,11 +11,59 @@ namespace Shuanglong::Utils
         
         struct tm *pLocalTime = localtime(&retTime);
 
-        std::cout << "---------------------" << std::endl;
-        std::cout << "year=" << pLocalTime->tm_year + 1900 << std::endl;
-        std::cout << "month=" << pLocalTime->tm_mon + 1 << std::endl;
-        std::cout << "day=" << pLocalTime->tm_mday << std::endl;
-        std::cout << "---------------------" << std::endl;
+        std::cout << "-------------------------------------------------------------------" << std::endl;
+        std::ostringstream sstring;
+        sstring << pLocalTime->tm_year + 1900;
+        sstring << "-";
+        int mon = pLocalTime->tm_mon + 1;
+        if (mon < 10)
+        {
+            sstring << "0";
+        }
+        sstring << mon;
+        sstring << "-";
+        int mday = pLocalTime->tm_mday;
+        if (mday < 10)
+        {
+            sstring << "0";
+        }
+        sstring << mday;
+        sstring << " ";
+        if (pLocalTime->tm_hour < 10)
+        {
+            sstring << "0";
+        }
+        sstring << pLocalTime->tm_hour;
+        sstring << ":";
+        if (pLocalTime->tm_min < 10)
+        {
+            sstring << "0";
+        }
+        sstring << pLocalTime->tm_min;
+        sstring << ":";
+        if (pLocalTime->tm_sec < 10)
+        {
+            sstring << "0";
+        }
+        sstring << pLocalTime->tm_sec;
+        std::cout << "CurrentTime: " << sstring.str() << std::endl;
+        std::cout << "-------------------------------------------------------------------" << std::endl;
+
+        time_t time1 = 100;
+        time_t time2 = 200;
+        double timeDiff = difftime(time2, time1);
+        std::cout << time2 << " - " << time1 << " = " << timeDiff << std::endl;
+        
+        struct timeval timeOfDay;
+        int res = gettimeofday(&timeOfDay, NULL);
+        if (res == 0)
+        {
+            std::cout << "timeOfDay: " << timeOfDay.tv_sec << std::endl;
+        }
+        else
+        {
+            std::cout << "gettimeofday failed: " << errno << std::endl;
+        }
 
         return retTime;
     }
