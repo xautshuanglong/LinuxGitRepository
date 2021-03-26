255afefa6a244dac1663c3e5052cc6c6a2cfa6ce
LinuxShuanglong <xjshuanglong@126.com>
2021-03-26 15:49:11 +0800

    补充 http 请求头部

--- PythonSpider/ip_util.py
+++ PythonSpider/ip_util.py
@@ -5,13 +5,25 @@ import requests
 
 
 class IpUtil:
+    # https://developer.amap.com/api/webservice/guide/api/ipconfig
     def gaode(ip_address: str):
-        payload = {
+        headers = {
+            'accept': 'application/json, text/javascript, */*; q=0.01',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'ec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest'
+        }
+        params = {
             'ip': ip_address,
             'key': 'fcb49ef9f062129a4a0aea6d3bec987f',
             'output': 'json'
         }
-        response = requests.get('https://restapi.amap.com/v3/ip', params=payload)
+        response = requests.get('https://restapi.amap.com/v3/ip', params=params, headers=headers)
         result = response.json()
 
         # print(response)
@@ -22,10 +34,23 @@ class IpUtil:
 
         return result;
 
+    # https://ip.taobao.com/ipSearch
     # 响应数据 code 值含义，0：成功  1：服务器异常  2：请求参数异常  3：服务器繁忙  4：个人qps超出。
     def taobao(ip_address: str):
-        payload = { 'ip': ip_address }
-        response = requests.get("https://ip.taobao.com/outGetIpInfo", params=payload)
+        headers = {
+            'accept': 'application/json, text/javascript, */*',
+            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'content-type': 'application/x-www-form-urlencoded',
+            'ec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest'
+        }
+        params = { 'ip': ip_address }
+        response = requests.get("https://ip.taobao.com/outGetIpInfo", params=params, headers=headers)
         result = response.json()
         return result
 
@@ -37,12 +62,13 @@ def main():
     print("    info      : %s" % (ip_gaode.get("info")))
     print("    infocode  : %s" % (ip_gaode.get("infocode")))
     print("    province  : %s" % (ip_gaode.get("province")))
+    print("    city      : %s" % (ip_gaode.get("city")))
     print("    adcode    : %s" % (ip_gaode.get("adcode")))
     print("    rectangle : %s" % (ip_gaode.get("rectangle")))
     ip_taobao = IpUtil.taobao(ip_address)
     print("ip info (gaode):")
-    print("    msg       : %s" % (ip_taobao.get("msg")))
-    print("    code      : %s" % (ip_taobao.get("code")))
+    print("    msg        : %s" % (ip_taobao.get("msg")))
+    print("    code       : %s" % (ip_taobao.get("code")))
     ip_taobao_data = ip_taobao.get('data', None)
     if ip_taobao_data is not None:
         print("    area       : %s" % (ip_taobao_data.get("area")))
